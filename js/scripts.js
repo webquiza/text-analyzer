a2b3da259256c7f59259947ba0992308bdd51794
Carlos Urquiza <webquiza@gmail.com>
2021-06-22 16:37:40 -0700

    update function to handle empty string input for word

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,7 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word, text) {
-  if (text.trim().length === 0) {
+  if ((text.trim().length === 0) || (word.trim().length === 0)) {
     return 0;
   }
   const wordArray = text.split(" ");

7aeb4cdd86d07d84b0edea88616847f0a860236f
Carlos Urquiza <webquiza@gmail.com>
2021-06-22 19:41:42 -0700

    add bold passage function with passing tests

--- js/scripts.js
+++ js/scripts.js
@@ -28,6 +28,22 @@ function numberOfOccurrencesInText(word, text) {
   return wordCount;
 }
 
+function boldPassage(word, text) {
+  let htmlString = "<p>";
+  let textArray = text.split(" ");
+  textArray.forEach(function(element, index) {
+    if (word === element) {
+      htmlString = htmlString.concat("<b>" + element + "</b>");
+    } else {
+      htmlString = htmlString.concat(element);
+    }
+    if (index !== (textArray.length - 1)) {
+      htmlString = htmlString.concat(" ");
+    }
+  });
+  return htmlString + "</p>";
+}
+
 // UI Logic
 
 $(document).ready(function(){
